5d5937db9f251a21a2de7337f4df110fd96ae8df
leviatan21 <leviatan21@gmail.com>
2025-02-20 16:33:22 -0300

    lanzamientos marzo 2025 v2

--- app/Http/Controllers/ReleasesController.php
+++ app/Http/Controllers/ReleasesController.php
@@ -16,6 +16,29 @@ class ReleasesController extends Controller {
         https://pdfobject.com/api/#pdfopenparams
         https://pdfobject.com/examples/pdf-open-params.html
         */
+
+        /*
+            <style>
+                .pdfobject-container { height: 100vh; border: 1px solid #ccc; }
+                </style>
+                <div id="efecto-gaudi-black-edition"></div>
+                <script src="https://unpkg.com/pdfobject"></script>
+                
+                <script>
+                    PDFObject.embed(
+                        '{{asset('/pdf/lanzamientos-efecto-gaudi-black-edition.pdf')}}',
+                        "#efecto-gaudi-black-edition",
+                        {
+                            'title':'Lanzamientos Marzo 2025',
+                            'forcePDFJS':false,
+                            'forceIframe':true,
+                            //'fallbackLink':false,
+                            'fallbackLink': "<p>Clique <a target='_blank' href='{{asset('/pdf/lanzamientos-efecto-gaudi-black-edition.pdf')}}'>aqui</a> si el PDF no se muestra</p>",
+                            'pdfOpenParams':{'view':'Fit', 'page':'1', 'zoom':'100', 'toolbar':0, 'statusbar':0}
+                        }
+                    );
+                </script>
+            */
         return view('releases.march2025');
     }
 }
